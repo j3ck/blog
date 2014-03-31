7467157b95cda7dea69489aaeea2a74db89229d2
j3ck <fox_raiodin@mail.ru>
2014-03-31 19:05:38 +0000

    scaffold posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SampleBlog::Application.routes.draw do
+  resources :posts
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
