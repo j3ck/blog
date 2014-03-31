64e7a7b17fa8ebf4b37c31224569c6d85ad890b4
j3ck <fox_raiodin@mail.ru>
2014-03-31 20:16:18 +0000

    added validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+
+	validates :title, presence: true
+	validates :title, length: { maximum: 100 }
+	validates :body, presence: true
+	validates :body, length: { minimum: 10 }
 end
