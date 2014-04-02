014990ed86b8761b3d94a256c0422e75802c9074
j3ck <fox_raiodin@mail.ru>
2014-04-02 22:24:11 +0000

    Fix sign_outs and added custom sign_out links

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 SampleBlog::Application.routes.draw do
-  devise_for :admins, path: "admin"
+  root 'home#index'
+
+  devise_for :admins, path: '/admin', controllers: { :sessions => "admin/sessions" }
 
   namespace :admin do
     root 'posts#index'
     resources :posts, except: :index
   end
 
-  root 'home#index'
 end
